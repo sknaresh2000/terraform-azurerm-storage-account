7485b1c55387dd6e2fea09cb05ed1ac62200f14f
NareshKumaran Sathiaseelan <naresh@nareshkransmini.mynetworksettings.com>
2023-03-28 12:39:15 -0400

    Added private endpoint details

--- main.tf
+++ main.tf
@@ -1,8 +1,40 @@
 resource "azurerm_storage_account" "sa" {
-  name                     = var.name
-  resource_group_name      = var.rg_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  tags                     = var.tags
+  name                             = var.name
+  resource_group_name              = var.rg_name
+  location                         = var.location
+  account_tier                     = var.account_tier
+  account_replication_type         = var.account_replication_type
+  cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
+  enable_https_traffic_only        = var.enable_https_traffic_only
+  allow_nested_items_to_be_public  = var.allow_nested_items_to_be_public
+  public_network_access_enabled    = var.public_network_access_enabled
+  tags                             = var.tags
+  dynamic "network_rules" {
+    for_each = var.public_network_access_enabled == false && var.network_acls != null ? [1] : []
+    content {
+      bypass                     = var.network_acls.bypass_services_info
+      default_action             = var.network_acls.default_action
+      ip_rules                   = var.network_acls.allowed_ips
+      virtual_network_subnet_ids = var.network_acls.service_endpoint_subnet_ids
+    }
+  }
+  dynamic "blob_properties" {
+    for_each = var.blob_versioning_enabled ? [1] : []
+    content {
+      versioning_enabled = var.blob_versioning_enabled
+    }
+  }
+}
+
+resource "azurerm_private_endpoint" "pe" {
+  name                = format("%s-%s", var.name, "private-endpoint")
+  location            = var.location
+  resource_group_name = var.rg_name
+  subnet_id           = var.private_endpoint_subnet_id
+  private_service_connection {
+    name                           = format("%s-%s", var.name, "private-service-connection")
+    private_connection_resource_id = azurerm_storage_account.sa.id
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.private_endpoint_sa_subresource_names
+  }
 }
--- variables.tf
+++ variables.tf
@@ -30,3 +30,59 @@ variable "tags" {
   type        = map(any)
   description = "Mapping of Tags"
 }
+
+variable "cross_tenant_replication_enabled" {
+  type        = bool
+  description = "Should cross Tenant replication be enabled?"
+  default     = false
+}
+
+variable "enable_https_traffic_only" {
+  type        = bool
+  description = "Boolean flag which forces HTTPS if enabled"
+  default     = true
+}
+
+variable "allow_nested_items_to_be_public" {
+  type        = bool
+  description = "Allow or disallow nested items within this Account to opt into being public"
+  default     = false
+}
+
+variable "public_network_access_enabled" {
+  type        = bool
+  description = "Whether the public network access is enabled?"
+  default     = false
+}
+
+variable "network_acls" {
+  type = object({
+    bypass_services_info        = list(string)
+    default_action              = string
+    allowed_ips                 = list(string)
+    service_endpoint_subnet_ids = list(string)
+  })
+  description = "Network & Firewall settings for storage account"
+}
+
+variable "blob_versioning_enabled" {
+  type        = bool
+  description = "Is blob versioning enabled"
+  default     = false
+}
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "Details of the subnet id where the private endpoint needs to be configured"
+}
+
+variable "is_manual_connection" {
+  type        = string
+  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
+  default     = false
+}
+
+variable "private_endpoint_sa_subresource_names" {
+  type        = list(string)
+  description = "A list of subresource names which the Private Endpoint is able to connect to."
+}
